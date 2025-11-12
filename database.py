da08d95f387dca5a971402697487eba16435a5a8
erzhan-kz <erzhan.sk.nqz@gmail.com>
2025-11-12 15:10:04 +0600

    Refactor database connection handling

--- database.py
+++ database.py
@@ -2,13 +2,9 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 import os
-from dotenv import load_dotenv
 
-load_dotenv()
-
-DATABASE_URL = os.getenv("DATABASE_URL")
-
-engine = create_engine(DATABASE_URL)
+DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./access_control.db")
+engine = create_engine(DATABASE_URL.replace("postgres://", "postgresql://"))
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
