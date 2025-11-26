ba62b7795f80af74c976bd16b391a475d4e8d0b2
erzhan-kz <erzhan.sk.nqz@gmail.com>
2025-11-26 21:19:52 +0600

    Change server port from 8000 to 10000

--- main.py
+++ main.py
@@ -166,4 +166,5 @@ def photo(filename: str):
     return FileResponse(path, media_type="image/jpeg")
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
+    app.run(host="0.0.0.0", port=10000)
+
